7d8ad4d34904a88d9b3b08d63d1cc56c354b6481
Orlando <orlandojosuetr@gmail.com>
2023-12-15 08:00:38 -0400

    Inicial Version

---
+++ gitClassOne.java
@@ -0,0 +1,20 @@
+package GIT;
+
+public class gitClassOne {
+
+	public void method1() {
+		
+		System.out.println(" method1 ");
+		
+	}
+	
+
+	public static void main(String[] args) {
+		
+		gitClassOne obj = new gitClassOne(); 
+		
+		obj.method1();
+
+	}
+
+}
